9ec602d163d6d5bbabc2510701106113b2da0d3e
James Wild <wildo543@hotmail.com>
2018-08-27 11:32:29 +0100

    Initial file

---
+++ main.py
@@ -0,0 +1,8 @@
+#/usr/bin/env python
+
+'''
+I get bored brushing my teeth. This script will search youtube until it finds a video that's two minutes long for me to watch while I scrub, to lessen the shitty experience that is dental care. 
+
+Maybe also add some reminder to do those tongue scrub things? Flossing too?
+'''
+
